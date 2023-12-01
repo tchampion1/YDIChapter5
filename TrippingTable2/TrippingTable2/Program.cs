8153ee99daf06a641a8b0e59b09bab9b91414015
tchampion1 <danea386@gmail.com>
2023-11-30 22:11:59 -0500

    add YDIChapter5 Dir and Files

---
+++ TrippingTable2/TrippingTable2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+
+namespace TippingTable2
+{
+    internal class TippingTable2
+    {
+        static void Main(string[] args)
+        {
+            double dinnerPrice = 10.00;
+            double tipRate;
+            double tip;
+            const double LOWRATE = 0.10;
+            const double MAXRATE = 0.25;
+            const double TIPSTEP = 0.05;
+            const double MAXDINNER = 100.00;
+            const double DINNERSTEP = 10.00;
+            const int NUM_DASHES = 40;
+
+            Write(" Price");
+            for (tipRate = LOWRATE; tipRate <= MAXRATE; tipRate += TIPSTEP)
+                Write($"{tipRate.ToString("F"),8}");
+            WriteLine();
+            //WriteLine("--------------------------------------------------");
+            for (int x = 0; x < NUM_DASHES; ++x)
+
+                Write("-");
+
+            WriteLine();
+
+            tipRate = LOWRATE;
+            do
+            {
+                Write($"{dinnerPrice.ToString("C")}");
+                while (tipRate <= MAXRATE)
+                {
+                    tip = dinnerPrice * tipRate;
+                    Write($"{tip.ToString("F"),8}");
+                    tipRate += TIPSTEP;
+
+                }
+                dinnerPrice += DINNERSTEP;
+                tipRate = LOWRATE;
+                WriteLine();
+
+            } while (dinnerPrice <= MAXDINNER);
+            ReadLine();
+
+        }
+
+    }
+}
---
+++ ValidID/ValidID/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace ValidID
+{
+    internal class ValidID
+    {
+        static void Main(string[] args)
+        {
+            int idNum;
+            string input;
+            const int LOW = 1000;
+            const int HIGH = 9999;
+
+            Write("Enter an ID number:");
+            input = Console.ReadLine();
+            idNum = Convert.ToInt32(input);
+
+            while (idNum < LOW || idNum > HIGH)
+            {
+                WriteLine($"{idNum} is an invalid ID number");
+                Write("ID numbers must be");
+                WriteLine($"between {LOW} and {HIGH} inclusive");
+                Write("Enter an ID number: ");
+                input = ReadLine();
+                idNum = Convert.ToInt32(input);
+                
+            }
+            WriteLine($"ID number {idNum} is valid");
+            ReadLine();
+        }
+    }
+}
